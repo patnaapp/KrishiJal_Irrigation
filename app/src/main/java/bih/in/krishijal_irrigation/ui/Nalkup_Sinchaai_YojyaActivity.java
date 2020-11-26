df71dd5aca129bcb04f0a9094d4a72d861b09ca2
patnaapp <patnaapp@gmail.com>
2020-11-26 15:04:00 +0530

    change

---
+++ app/src/main/java/bih/in/krishijal_irrigation/ui/Nalkup_Sinchaai_YojyaActivity.java
@@ -0,0 +1,16 @@
+package bih.in.krishijal_irrigation.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bih.in.krishijal_irrigation.R;
+
+public class Nalkup_Sinchaai_YojyaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nalkup__sinchaai__yojya);
+    }
+}
